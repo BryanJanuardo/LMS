5131984cbdeab113d0b97632d582c7424d2077fe
Michelle Amabel <96010198+misyelel@users.noreply.github.com>
2024-12-02 19:50:49 +0700

    task

--- app/Models/task.php
+++ app/Models/task.php
@@ -8,18 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
-
-    protected $table = 'tasks';
-
-    protected $fillable = [
-        'TaskName',
-        'TaskDesc',
-        'TaskType',
-        'TaskDueDate',
-    ];
-
-    public function sessionLearnings()
-    {
-        return $this->hasMany(SessionLearning::class, 'TaskID'); // foreign key: 'TaskID'
-    }
 }
