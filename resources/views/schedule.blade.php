bb866772ed1df09e606bf569908c77ff3a8f8703
Michelle Amabel <96010198+misyelel@users.noreply.github.com>
2024-12-03 20:11:16 +0700

    added features to resources card

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,34 +1,3 @@
-{{-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Course Schedules - {{ $semester }}</title>
-</head>
-<body>
-    <h1>Course Schedules for {{ $semester }}</h1>
-
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Course Code</th>
-                <th>Course Title</th>
-                <th>Day</th>
-                <th>Time</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($schedules as $schedule)
-                <tr>
-                    <td>{{ $schedule['courseCode'] }}</td>
-                    <td>{{ $schedule['title'] }}</td>
-                    <td>{{ $schedule['sessionStart'] }}</td>
-                    <td>{{ $schedule['sessionEnd'] }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</body>
-</html> --}}
-
 @extends('layout')
 
 @section('title', 'Schedule')
