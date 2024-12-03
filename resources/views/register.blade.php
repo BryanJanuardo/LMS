40b45bc66e5a38c20eeb7e4dc24742b5879d57d8
Michelle Amabel <96010198+misyelel@users.noreply.github.com>
2024-12-03 14:11:06 +0700

    login register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,36 @@
+@extends('guest-layout')
+
+@section('title', 'Register')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <h2 class="text-center mb-4">Register</h2>
+        <form>
+            <div class="mb-3">
+                <label for="UserName" class="form-label">Name</label>
+                <input type="text" class="form-control" id="UserName" name="UserName" placeholder="Enter your name">
+            </div>
+            <div class="mb-3">
+                <label for="UserEmail" class="form-label">Email</label>
+                <input type="email" class="form-control" id="UserEmail" name="UserEmail" placeholder="Enter your email">
+            </div>
+            <div class="mb-3">
+                <label for="UserPassword" class="form-label">Password</label>
+                <input type="password" class="form-control" id="UserPassword" name="UserPassword" placeholder="Enter your password">
+            </div>
+            <div class="mb-3">
+                <label for="UserDOB" class="form-label">Date of Birth</label>
+                <input type="date" class="form-control" id="UserDOB" name="UserDOB">
+            </div>
+            <div class="mb-3">
+                <label for="UserPhotoPath" class="form-label">Photo</label>
+                <input type="file" class="form-control" id="UserPhotoPath" name="UserPhotoPath" accept="image/*">
+            </div>
+            <div class="d-grid">
+                <button type="button" class="btn btn-primary">Register</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
