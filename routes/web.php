923360cb21b32f8326a379223b34af7e94569924
BryanJanuardo <bryan.januardo1401@gmail.com>
2024-12-07 14:08:33 +0700

    Merge Task/Material/Quiz

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\ForumController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\CourseDetailController;
+use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
@@ -44,8 +45,21 @@ Route::prefix('/course')->group(function () {
         Route::put('/{SessionID}', [SessionController::class, 'update'])->name('session.update');
         Route::delete('/{SessionID}', [SessionController::class, 'delete'])->name('session.delete');
 
+        Route::prefix('{SessionID}/material')->group(function () {
+            Route::get('/create', [MaterialController::class, 'create'])->name('material.create');
+            Route::post('/store', [MaterialController::class, 'store'])->name('material.store');
+            Route::get('/add', [MaterialController::class, 'add'])->name('material.add');
+            Route::put('/{MaterialID}', [MaterialController::class, 'update'])->name('material.update');
+            Route::delete('/{MaterialID}', [MaterialController::class, 'destroy'])->name('material.destroy');
+            Route::get('/edit/{MaterialID}', [MaterialController::class, 'edit'])->name('material.edit');
+        });
         Route::prefix('{SessionID}/task')->group(function () {
-            Route::get('/task', [TaskController::class, 'index'])->name('tasks.index');
+            Route::get('/create', [TaskController::class, 'create'])->name('task.create');
+            Route::post('/store', [TaskController::class, 'store'])->name('task.store');
+            Route::get('/add', [TaskController::class, 'add'])->name('task.add');
+            Route::put('/{TaskID}', [TaskController::class, 'update'])->name('task.update');
+            Route::delete('/{TaskID}', [TaskController::class, 'destroy'])->name('task.destroy');
+            Route::get('/edit/{TaskID}', [TaskController::class, 'edit'])->name('task.edit');
         });
 
         Route::prefix('/{SessionID}/forum')->group(function () {
